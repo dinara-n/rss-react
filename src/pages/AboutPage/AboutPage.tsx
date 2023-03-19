95add973315b4a5ac148292ee9fcec8ad23265ea
Dinara <dinaranav1987@gmail.com>
2023-03-20 01:38:41 +0400

    feat: add pages

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About us</h1>
+      <p>This page contains some information about us.</p>
+    </>
+  );
+}
+
+export default AboutPage;
---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFoundPage() {
+  return (
+    <>
+      <h1>Error 404: Page Not Found</h1>
+      <p>The requested page does not seem to exist.</p>
+    </>
+  );
+}
+
+export default NotFoundPage;
