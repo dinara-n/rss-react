298b09a940997deccb2a6a8aa25c3823d8939551
Dinara <dinaranav1987@gmail.com>
2023-03-20 03:02:57 +0400

    feat: add SearchBar

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 function CatalogPage() {
   return (
     <>
       <h1>Catalog</h1>
+      <SearchBar />
     </>
   );
 }
