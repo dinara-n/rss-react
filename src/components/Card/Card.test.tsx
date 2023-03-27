005a470f82b0ee3d7aca568c74ac39eeb8f120d0
Dinara <dinaranav1987@gmail.com>
2023-03-27 06:15:21 +0400

    feat: add tests

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -3,16 +3,32 @@ import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import Card from './Card';
-import { cardsData } from '../../assets/data';
+import lukeSkywalker from './Luke_skywalker.jpg';
+import { CharacterGender, CharacterSpecies } from 'types/types';
+
+const cardData = {
+  name: 'Luke Skywalker',
+  height: '172',
+  mass: '77',
+  hair_color: 'blond',
+  skin_color: 'fair',
+  eye_color: 'blue',
+  birth_year: '19BBY',
+  species: 'human' as CharacterSpecies,
+  gender: 'male' as CharacterGender,
+  url: 'https://swapi.dev/api/people/1/',
+  image: lukeSkywalker,
+};
 
 describe('Card', () => {
   it('Renders card', () => {
-    render(<Card cardData={cardsData[0]} />);
-    expect(screen.getAllByRole('heading', { level: 3 }));
+    render(<Card cardData={cardData} />);
+    expect(screen.getByRole('heading', { level: 3 })).toHaveTextContent(/Luke Skywalker/i);
     expect(screen.getAllByRole('img'));
-    expect(screen.getAllByText('Height:'));
-    expect(screen.getAllByText('Mass:'));
-    expect(screen.getAllByText('Date of birth:'));
-    expect(screen.getAllByText('Gender:'));
+    expect(screen.getByText('Height:')).toBeInTheDocument();
+    expect(screen.getByText('Mass:')).toBeInTheDocument();
+    expect(screen.getByText('Date of birth:')).toBeInTheDocument();
+    expect(screen.getByText('Species:')).toBeInTheDocument();
+    expect(screen.getByText('Gender:')).toBeInTheDocument();
   });
 });
