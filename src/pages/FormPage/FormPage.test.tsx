08675f61f24a9e33638f293018898dac646d5f16
Dinara <dinaranav1987@gmail.com>
2023-04-17 04:09:25 +0400

    feat: update tests

--- src/pages/FormPage/FormPage.test.tsx
+++ src/pages/FormPage/FormPage.test.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 
 import FormPage from './FormPage';
+import store from '../../store/store';
 
 describe('FormPage', () => {
   it('renders the cards list', () => {
-    render(<FormPage />);
+    render(
+      <Provider store={store}>
+        <FormPage />
+      </Provider>
+    );
     expect(screen.getByText(/No cards to display/i)).toBeInTheDocument();
   });
 });
